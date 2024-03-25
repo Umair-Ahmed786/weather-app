a966768555288556838c2c14f2329d9dcb2842a9
Tanay Patel <tanaydpatel@gmail.com>
2024-03-25 22:12:53 +0530

    ⚡ weather and localstorage

--- src/app/components/Weather.jsx
+++ src/app/components/Weather.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
-import { Box, Center, Loader, Text, Title } from "@mantine/core";
+import ReactAnimatedWeather from "react-animated-weather";
+import {
+  Box,
+  Center,
+  Grid,
+  Image,
+  Loader,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
 
 import LocationInput from "./LocationInput";
 
@@ -27,7 +37,62 @@ function Weather({
           </Center>
         ) : (
           <Box my="xl">
-            <pre>{JSON.stringify(weatherData, null, 2)}</pre>
+            {weatherData ? (
+              <>
+                <Title>{weatherData?.name}</Title>
+                <Stack gap={0}>
+                  <Center>
+                    <Image
+                      h={100}
+                      w={100}
+                      src={`https://openweathermap.org/img/wn/${weatherData?.weather?.[0].icon}@2x.png`}
+                      alt={weatherData?.weather?.[0].description}
+                      title={weatherData?.weather?.[0].description}
+                    />
+                    <div>
+                      <Text size="30px" super fw={600}>
+                        {weatherData?.main?.temp} <sup>°C</sup>
+                      </Text>
+                    </div>
+                  </Center>
+                  <Text size="sm">
+                    Temp. range{" "}
+                    <strong>
+                      {weatherData?.main?.temp_min}
+                      <sup>°C</sup> - {weatherData?.main?.temp_max}
+                      <sup>°C</sup>
+                    </strong>
+                  </Text>
+                  <Grid my="lg">
+                    <Grid.Col span={{ md: 6, xs: 12 }}>
+                      <div className="weather-condition">
+                        <div>
+                          <ReactAnimatedWeather icon="RAIN" size="40" />
+                        </div>
+                        <div>
+                          <Text fw={600}>{weatherData?.main?.humidity} %</Text>
+                          <Text size="sm">Humidity</Text>
+                        </div>
+                      </div>
+                    </Grid.Col>
+                    <Grid.Col span={{ md: 6, xs: 12 }}>
+                      <div className="weather-condition">
+                        <div>
+                          <ReactAnimatedWeather icon="WIND" size="40" />
+                        </div>
+                        <div>
+                          <Text fw={600}>{weatherData?.wind.speed} m/s</Text>
+                          <Text size="sm">Wind</Text>{" "}
+                        </div>
+                      </div>
+                    </Grid.Col>
+                  </Grid>
+                  <Text fw={600} c="cyan" tt="capitalize" size="lg">
+                    {weatherData?.weather?.[0].description}
+                  </Text>
+                </Stack>
+              </>
+            ) : null}
             {error ? (
               <>
                 <Title>{error.response.status}!</Title>
--- src/app/data/weather/index.js
+++ src/app/data/weather/index.js
@@ -2,12 +2,24 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 const useWeather = ({ location }) => {
-  console.log(process.env.REACT_APP_OPENWEATHERMAP_API_KEY);
   const fetchWeather = async () => {
     if (!location) return null;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`;
-    const response = await axios.get(url);
-    return response.data;
+    const localData = JSON.parse(localStorage.getItem("weather") || "{}");
+
+    if (localData?.[location]) {
+      return localData?.[location];
+    } else {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}&units=metric`;
+      const response = await axios.get(url);
+      localStorage.setItem(
+        "weather",
+        JSON.stringify({
+          ...localData,
+          [location]: response.data,
+        })
+      );
+      return response.data;
+    }
   };
 
   const { isLoading, error, data } = useQuery({
